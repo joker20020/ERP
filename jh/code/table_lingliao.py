2eef8ccafc8d537215a9ad108b880f19211549bb
18310648172 <Jackiekingli@163.com>
2023-10-26 11:52:20 +0800

    10.26

--- jh/code/table_lingliao.py
+++ jh/code/table_lingliao.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'table-LingLiao.ui'
+## Form generated from reading UI file 'table-lingliao.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.4.2
 ##
@@ -23,7 +23,7 @@ class table_lingliao(object):
     def setupUi(self, Form):
         if not Form.objectName():
             Form.setObjectName(u"Form")
-        Form.resize(515, 417)
+        Form.resize(643, 441)
         self.verticalLayout = QVBoxLayout(Form)
         self.verticalLayout.setObjectName(u"verticalLayout")
         self.label = QLabel(Form)
@@ -34,14 +34,16 @@ class table_lingliao(object):
         self.verticalLayout.addWidget(self.label)
 
         self.tableWidget = QTableWidget(Form)
-        if (self.tableWidget.columnCount() < 3):
-            self.tableWidget.setColumnCount(3)
+        if (self.tableWidget.columnCount() < 4):
+            self.tableWidget.setColumnCount(4)
         __qtablewidgetitem = QTableWidgetItem()
         self.tableWidget.setHorizontalHeaderItem(0, __qtablewidgetitem)
         __qtablewidgetitem1 = QTableWidgetItem()
         self.tableWidget.setHorizontalHeaderItem(1, __qtablewidgetitem1)
         __qtablewidgetitem2 = QTableWidgetItem()
         self.tableWidget.setHorizontalHeaderItem(2, __qtablewidgetitem2)
+        __qtablewidgetitem3 = QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(3, __qtablewidgetitem3)
         if (self.tableWidget.rowCount() < 20):
             self.tableWidget.setRowCount(20)
         self.tableWidget.setObjectName(u"tableWidget")
@@ -62,10 +64,12 @@ class table_lingliao(object):
         Form.setWindowTitle(QCoreApplication.translate("Form", u"Form", None))
         self.label.setText(QCoreApplication.translate("Form", u"\u9886\u6599\u5355", None))
         ___qtablewidgetitem = self.tableWidget.horizontalHeaderItem(0)
-        ___qtablewidgetitem.setText(QCoreApplication.translate("Form", u"\u7269\u6599ID", None));
+        ___qtablewidgetitem.setText(QCoreApplication.translate("Form", u"\u5de5\u4f5c\u4e2d\u5fc3ID", None));
         ___qtablewidgetitem1 = self.tableWidget.horizontalHeaderItem(1)
-        ___qtablewidgetitem1.setText(QCoreApplication.translate("Form", u"\u9700\u6c42\u91cf", None));
+        ___qtablewidgetitem1.setText(QCoreApplication.translate("Form", u"\u7269\u6599ID", None));
         ___qtablewidgetitem2 = self.tableWidget.horizontalHeaderItem(2)
-        ___qtablewidgetitem2.setText(QCoreApplication.translate("Form", u"\u9700\u6c42\u65f6\u95f4", None));
+        ___qtablewidgetitem2.setText(QCoreApplication.translate("Form", u"\u9700\u6c42\u91cf", None));
+        ___qtablewidgetitem3 = self.tableWidget.horizontalHeaderItem(3)
+        ___qtablewidgetitem3.setText(QCoreApplication.translate("Form", u"\u9700\u6c42\u65f6\u95f4", None));
     # retranslateUi
 
