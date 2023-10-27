16e7ba9eef3458fbad01a6470cba70dff9dc24a6
xxx-buaa <1175779621@qq.com>
2023-10-27 18:44:44 +0800

    采购模块10月27日更新

--- cg/cg_widget.py
+++ cg/cg_widget.py
@@ -8,14 +8,14 @@
 
 TODO: 貌似很多头函数没用上，后续可能会删除或者精简一下
 TODO: 检查所有文件的路径、相对路径/绝对路径、是否有同名文件、是否最新，以及文件名
-TODO: 下一步可能需要完善功能，然后进行代码优化、改bug
 
 TODO: 
-0.完成日志功能 
-1.完善功能：接受请购，发送收获数据
-2.供应商评价(功能和bug) 
-3.界面美化，使用qfluetwidget库重新生成ui 
+0.完成日志功能(完成)
+1.完善功能：接受请购，发送收获数据(完成)
+2.供应商评价页面(功能和bug) 
+3.界面美化，使用qfluetwidget库重新生成ui(完成)
 
+FIXME: 库存模块的类传入时，路径好像有问题 LINE402 LINE403
 UPDATE: 数据库文件采用绝对路径传入 line50
 UPDATE: 所有图标等资源文件建议放入项目根目录下res文件夹再使用
 '''
@@ -26,6 +26,7 @@ import time
 
 sys.path.append(os.path.abspath("xt/code"))
 sys.path.append(os.path.abspath("kc"))
+sys.path.append(os.path.abspath("jh/code"))
 
 from PySide6.QtWidgets import (QApplication, QWidget, QComboBox,
 QPushButton, QLabel, QLineEdit, QHBoxLayout, QVBoxLayout, QDialog,
@@ -33,24 +34,24 @@ QGroupBox, QMainWindow, QRadioButton, QGridLayout, QTableView,
 QFormLayout, QStackedLayout, QScrollArea, QFileDialog, 
 QGraphicsView, QGraphicsScene, QGraphicsPixmapItem)
 from PySide6.QtUiTools import QUiLoader
-from PySide6.QtCore import Qt, QThread, Signal, Slot, QModelIndex
-from PySide6.QtGui import QIcon, QStandardItem, QStandardItemModel, QPixmap, QPainter, QImage
+from PySide6.QtCore import Qt, QThread, Signal, Slot, QModelIndex, QDateTime
+from PySide6.QtGui import QIcon, QStandardItem, QStandardItemModel, QPixmap, QPainter, QImage, QDoubleValidator
 from PySide6.QtSql import QSqlDatabase, QSqlQuery, QSqlTableModel
 from PySide6 import QtCore
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 
+from cg_database import *
 from cg_ui.version3 import Ui_cg_sector
 from cg_ui.dialog import Ui_cg_form
 from xt_container import OperationCode, XtContainer
 from inventory import InventoryManager
 
-# import cg_database
 
 # 这是生成采购部门模块的类
 class cg_widget(QWidget):
     # 继承父类，并执行类的方法，一些基础的设定
-    def __init__(self, detail_file, list_file, receipt_file, supplier_file, icon = "cg_ui/u102.png"):
+    def __init__(self, detail_file, list_file, receipt_file, supplier_file, user_name, xt_log):
         super().__init__()
         # 定义窗体大小
         self.resize(800, 480)
@@ -65,13 +66,14 @@ class cg_widget(QWidget):
         self.supplier_file = supplier_file
 
         # OC = OperationCode()
-        # XC = XtContainer()
+        self.log = XtContainer(1, xt_log, user_name)
 
         # 设定左上角图标，图标png文件使用绝对路径
-        icon = QIcon(os.path.abspath(icon))
-        self.setWindowIcon(icon)
+        # icon = QIcon(os.path.abspath(icon))
+        # self.setWindowIcon(icon)
         # 定义一个线程的状态
         self.thread_running = False
+        self.selected_data = []
 
     # 一些初始化操作
     def init_ui(self):
@@ -82,6 +84,7 @@ class cg_widget(QWidget):
 
         # 现在可以直接绑定槽函数
         self.ui.tabWidget.currentChanged.connect(self.tab_changed)
+        self.is_btn_receive_connected = False
 
     # 识别当前窗口的槽函数，并跳转到相应页面的初始化函数
     @Slot()
@@ -123,6 +126,7 @@ class cg_widget(QWidget):
         self.ui.receive_add.clicked.connect(self.receive_add_table)
         self.ui.receive_delete.clicked.connect(self.receive_delete_table)
         self.ui.receive_save.clicked.connect(self.receive_save_table)
+        self.ui.receive_btn.clicked.connect(self.receive_to_inver)
 
     # 4.供应商管理与评价页面
     def init_supplier(self):
@@ -174,11 +178,11 @@ class cg_widget(QWidget):
         # TODO: 写这个代码块的注释
         # 设置tableView的模型以及表的名称
         self.model = QSqlTableModel(self, self.database)
-        self.model.setTable('cg_purchase_list')
+        self.model.setTable('MPS_table')
 
-        self.model.setHeaderData(0, Qt.Horizontal, "采购请求号")
-        self.model.setHeaderData(1, Qt.Horizontal, "计划分配号")
-        self.model.setHeaderData(2, Qt.Horizontal, "备注")
+        self.model.setHeaderData(0, Qt.Horizontal, "物料编码")
+        self.model.setHeaderData(1, Qt.Horizontal, "计划订购量")
+        self.model.setHeaderData(2, Qt.Horizontal, "交付最后日期")
         self.model.select()
         # 显示表格
         self.ui.requisition_table_view.setModel(self.model)
@@ -188,14 +192,31 @@ class cg_widget(QWidget):
         self.selected_row = None
         self.ui.requisition_table_view.selectionModel().selectionChanged.connect(self.handle_selection_change)
 
+        self.log.generate_log(OperationCode.CG_CHANGE)
+
     # TODO: 写这个代码块的注释
     # 1的槽函数：选择某行并返回索引
     # 已经在Qt Designer中设置好了每行数据只读，并且按行读，所以直接返回下标就可以
     @Slot()
     def handle_selection_change(self, selected):
         if selected.indexes():
+            self.selected_data = []
+            index = selected.indexes()[0]
             selected_row = selected.indexes()[0].row()
             self.selected_row = selected_row
+
+            model = self.ui.requisition_table_view.model()
+
+            product_id = model.data(model.index(index.row(), 0))
+            planned_amount = model.data(model.index(index.row(), 1))
+            planned_deadline = model.data(model.index(index.row(), 2))
+
+            self.selected_data.append((str(product_id), str(planned_amount), str(planned_deadline)))
+        
+        else:
+            self.selected_data = []
+
+        del index
 
     # TODO: 写这个代码块的注释，以及实现子线程函数操作
     # 1的槽函数：选择某行后再点击确认按钮，就执行此操作
@@ -203,8 +224,21 @@ class cg_widget(QWidget):
     @Slot()
     def accept_requisition(self):
         if self.selected_row is not None:
+            print("table: ", detail_file)
             # You can now use self.selected_row to access the selected row
             print(f"Selected Row: {self.selected_row}")
+            print(f"selected data is: ", self.selected_data)
+            conn = sqlite3.connect(detail_file)
+            cursor = conn.cursor()
+            cursor.execute("SELECT MAX(cg_purchase_detail_id) FROM cg_purchase_detail")
+            max_id = cursor.fetchone()[0]
+            add_requisition = cg_purchase_detail(detail_file, 'cg_purchase_detail', 'cg_purchase_detail_id')
+            add_requisition.add_item(
+                cg_purchase_detail_id = max_id + 1,
+                material_code = self.selected_data[0][0],
+                cg_order_lot = self.selected_data[0][1],
+                cg_order_time = self.selected_data[0][2]
+            )
         else:
             print("No row selected")
 
@@ -221,16 +255,20 @@ class cg_widget(QWidget):
             model.setTable(table_name)
 
             model.setHeaderData(0, Qt.Horizontal, "采购明细号")
-            model.setHeaderData(1, Qt.Horizontal, "采购清单号")
-            model.setHeaderData(2, Qt.Horizontal, "物料编码")
-            model.setHeaderData(3, Qt.Horizontal, "订货批量")
-            model.setHeaderData(4, Qt.Horizontal, "商品总价")
-            model.setHeaderData(5, Qt.Horizontal, "收货单号")
-            model.setHeaderData(6, Qt.Horizontal, "备注")
+            model.setHeaderData(1, Qt.Horizontal, "物料编码")
+            model.setHeaderData(2, Qt.Horizontal, "订货批量")
+            model.setHeaderData(3, Qt.Horizontal, "商品总价")
+            model.setHeaderData(4, Qt.Horizontal, "收货单号")
+            model.setHeaderData(5, Qt.Horizontal, "预计到货时间")
+            model.setHeaderData(6, Qt.Horizontal, "供应商")
+            model.setHeaderData(7, Qt.Horizontal, "备注")
             model.select()
             # 显示表格
             self.ui.order_table_view.setModel(model)
             self.ui.order_table_view.resizeColumnsToContents()
+        
+        self.ui.order_table_view.selectionModel().selectionChanged.connect(self.order_table_changed)
+        self.log.generate_log(OperationCode.CG_CHANGE)
 
     # 2的槽函数
     def order_add_table(self):
@@ -258,6 +296,46 @@ class cg_widget(QWidget):
         else:
             model.database().rollback()
 
+    @Slot()
+    def order_table_changed(self, selected, deselected):
+        validator = QDoubleValidator()
+        validator.setDecimals(2)
+        if selected.indexes():
+            index = selected.indexes()[0]
+
+            model = self.ui.order_table_view.model()
+
+            detail_id = model.data(model.index(index.row(), 0))
+            material_code = model.data(model.index(index.row(), 1))
+            order_lot = model.data(model.index(index.row(), 2))
+            total_price = model.data(model.index(index.row(), 3))
+            receipt_id = model.data(model.index(index.row(), 4))
+            order_time = model.data(model.index(index.row(), 5))
+            order_supplier = model.data(model.index(index.row(), 6))
+            remarks = model.data(model.index(index.row(), 7))
+                       
+            self.ui.order_total_price.setValidator(validator)
+            datetime = QDateTime.fromString(order_time, "yyyy-MM-dd HH:mm:ss")
+
+            self.ui.order_detail_id.setText(str(detail_id))
+            self.ui.order_bom.setText(str(material_code))
+            self.ui.order_quantity.setText(str(order_lot))
+            self.ui.order_total_price.setText(str(total_price))
+            self.ui.order_receipt_box.setText(str(receipt_id))
+            self.ui.dateTimeEdit.setDateTime(datetime)
+            self.ui.order_supplier.setText(str(order_supplier))
+            self.ui.order_remarks.setText(str(remarks))
+           
+        else:
+            self.ui.order_detail_id.clear()  
+            self.ui.order_bom.clear()
+            self.ui.order_quantity.clear()
+            self.ui.order_total_price.clear()
+            self.ui.order_receipt_box.clear()    
+            self.ui.dateTimeEdit.setDateTime(QDateTime.currentDateTime())
+            self.ui.order_supplier.clear()   
+            self.ui.order_remarks.clear()
+
     # 3的槽函数
     def receive_table(self):
         self.database = QSqlDatabase.addDatabase("QSQLITE")
@@ -271,15 +349,19 @@ class cg_widget(QWidget):
             model.setTable(table_name)
 
             model.setHeaderData(0, Qt.Horizontal, "收货单号")
-            model.setHeaderData(1, Qt.Horizontal, "到货时间")
-            model.setHeaderData(2, Qt.Horizontal, "到货量")
-            model.setHeaderData(3, Qt.Horizontal, "合格品数")
-            model.setHeaderData(4, Qt.Horizontal, "入库状态")
+            model.setHeaderData(1, Qt.Horizontal, "物料编码")
+            model.setHeaderData(2, Qt.Horizontal, "到货时间")
+            model.setHeaderData(3, Qt.Horizontal, "到货量")
+            model.setHeaderData(4, Qt.Horizontal, "合格品数")
+            model.setHeaderData(5, Qt.Horizontal, "入库状态")
             model.setHeaderData(6, Qt.Horizontal, "备注")
             model.select()
             # 显示表格
             self.ui.receive_table_view.setModel(model)
             self.ui.receive_table_view.resizeColumnsToContents()
+
+        self.ui.receive_table_view.selectionModel().selectionChanged.connect(self.receive_table_changed)
+        self.log.generate_log(OperationCode.CG_CHANGE)
 
     # 3的槽函数
     def receive_add_table(self):
@@ -306,6 +388,68 @@ class cg_widget(QWidget):
             model.database().commit()
         else:
             model.database().rollback()
+
+    @Slot()
+    def receive_to_inver(self):
+        row = self.index
+        # if not self.is_btn_receive_connected:
+        #     self.ui.receive_btn.clicked.disconnect(self.receive_to_inver)
+        #     self.is_btn_receive_connected = True
+        if self.ui.receive_status.isChecked():
+            print("ok, the row is: ", row)
+            print("the data is:", self.selected_data)
+
+            rec_to_inv = InventoryManager()
+            rec_to_inv.add_inventory(self.selected_data[0][0], self.selected_data[0][0], self.selected_data[0][0], "采购")
+        else:
+            print("not be chosen")
+
+    @Slot()
+    def receive_table_changed(self, selected, deselected):
+        if selected.indexes():
+            self.selected_data = []
+            # Get the selected row's index
+            index = selected.indexes()[0]
+            self.index = selected.indexes()[0].row()
+            
+            # Assuming you have a QSqlTableModel, you can access the data in the model
+            model = self.ui.receive_table_view.model()
+            
+            # Extract data from the model for the selected row and column
+            receipt_id = model.data(model.index(index.row(), 0))
+            material_code = model.data(model.index(index.row(), 1))
+            arrival_time = model.data(model.index(index.row(), 2))
+            arrival_quantity = model.data(model.index(index.row(), 3))
+            qualified_products = model.data(model.index(index.row(), 4))
+            inbound_status = model.data(model.index(index.row(), 5))
+            remarks = model.data(model.index(index.row(), 6))
+
+            datetime = QDateTime.fromString(arrival_time, "yyyy-MM-dd HH:mm:ss")
+            
+            # Update the QLineEdit and QTextBrowser
+            self.ui.receive_id.setText(str(receipt_id))
+            self.ui.receive_item.setText(str(material_code))
+            self.ui.receive_time.setDateTime(datetime)
+            self.ui.receive_quantity.setText(str(arrival_quantity))
+            self.ui.receive_qualified.setText(str(qualified_products))
+            self.ui.receive_remarks.setText(str(remarks))
+
+            if inbound_status == 1:
+                self.ui.receive_status.setChecked(True)
+            else:
+                self.ui.receive_status.setChecked(False)
+            
+            self.selected_data.append((str(arrival_time), str(material_code), str(arrival_quantity)))
+
+        else:
+            # Clear the QLineEdit and QTextBrowser if no row is selected
+            self.ui.receive_id.clear()
+            self.ui.receive_time.setDateTime(QDateTime.currentDateTime())
+            self.ui.receive_quantity.clear()
+            self.ui.receive_qualified.clear()
+            self.ui.receive_remarks.clear()
+            self.ui.receive_status.clear()
+            self.selected_data = []
 
     # FIXME: 有bug，但是暂时不想改了
     # 4的槽函数：列举供应商信息
@@ -335,6 +479,7 @@ class cg_widget(QWidget):
             self.ui.supplier_list.clicked.connect(lambda: self.supplier_table_view(company_id))
             # if self.ui.supplier_list.clicked:
             #   print("OKKKKKK!!!! This is: ", company_id)
+        self.log.generate_log(OperationCode.CG_CHANGE)
     
     # TODO: 写这个代码块的注释
     # FIXME: 供应商模块还有bug
@@ -357,7 +502,12 @@ class cg_widget(QWidget):
         del scene
 
     def supplier_table_view(self, company_id):
-        dialog = supplier_form(company_id)
+        sup1 = os.path.abspath("cg/cg_db/40004001.db")
+        sup2 = os.path.abspath("cg/cg_db/40004002.db")
+        sup3 = os.path.abspath("cg/cg_db/40004003.db")
+        sup4 = os.path.abspath("cg/cg_db/40004004.db")
+        sup5 = os.path.abspath("cg/cg_db/40004005.db")
+        dialog = supplier_form(company_id, sup1, sup2, sup3, sup4, sup5)
         dialog.exec()
 
     # TODO: 写这个代码块
@@ -417,6 +567,7 @@ class cg_widget(QWidget):
             # 显示表格
             self.ui.query_list_view.setModel(model)
             self.ui.query_list_view.resizeColumnsToContents() 
+        self.log.generate_log(OperationCode.CG_CHANGE)
 
     def detect_table_name(self):
         tables = self.database.tables()
@@ -469,17 +620,18 @@ class supplier_eval(QThread):
             self.quit()
 
 class supplier_form(QDialog):
-    def __init__(self, company_id):
+    def __init__(self, company_id, sup1, sup2, sup3, sup4, sup5):
         super(supplier_form, self).__init__()
         self.ui = Ui_cg_form()
         self.ui.setupUi(self)
         self.company_id = company_id
         self.init_table()
+        self.supplier_table = sup1
     
     def init_table(self):
         company_id = self.company_id
         self.database = QSqlDatabase.addDatabase("QSQLITE")
-        filename = f"cg_db/{company_id}.db"
+        filename = self.supplier_table
         self.database.setDatabaseName(filename)
         if not self.database.open():
             print("Error: Could not open the database")
@@ -538,14 +690,14 @@ class supplier_form(QDialog):
 
 if __name__ == '__main__':
     detail_file = os.path.abspath("cg/cg_db/Purchase Detail.db")
-    list_file = os.path.abspath("cg/cg_db/Purchase List.db")
+    list_file = os.path.abspath("jh/code/JHdatabase.db")
     receipt_file = os.path.abspath("cg/cg_db/Purchase Receipt.db")
     supplier_file = os.path.abspath("cg/cg_db/Purchase Supplier.db")
 
     # 创建一个名为app的实例，代表应用本身，用于设置GUI并处理事件
     app = QApplication(sys.argv)
     # 实例化MyWindow
-    widget = cg_widget(detail_file, list_file, receipt_file, supplier_file)
+    widget = cg_widget(detail_file, list_file, receipt_file, supplier_file, '../test.db', "采购")
     # 在屏幕上显示QWiget窗口
     widget.show()
     # 启动QApplication的循环，直到用户关闭窗口
