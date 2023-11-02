e3e5ab035e61c342b83c1f68b4a85ac083592999
d'y j <405997563@qq.comgit config --global user.name joker20020>
2023-11-02 09:38:25 +0800

    realese1.5

--- kc/inventory.py
+++ kc/inventory.py
@@ -141,7 +141,7 @@ class InventoryManager:
 # 示例用法
 if __name__ == "__main__":
     manager = InventoryManager('inventory.db')
-    # manager.c.execute("DROP TABLE chuku")
+    # manager.c.execute("DROP TABLE ruku")
 
     # 查询使用方法：
     db = InventoryManager('inventory.db')
